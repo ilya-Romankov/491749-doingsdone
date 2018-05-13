4062c8d0327a0a6821c255412f9d0234bdab8a65
Ilya <mordar14@yadnex.ru>
2018-05-14 02:10:05 +0700

    Исправил БД

--- schema.sql
+++ schema.sql
@@ -9,24 +9,6 @@ CREATE TABLE projects (
     name_projects CHAR(32)
 );
 
-CREATE TABLE task (
-    id_task INT AUTO_INCREMENT PRIMARY KEY,
-    id_users INT,
-    id_project INT,
-    id_projects INT NOT NULL,
-    date_create_task DATE,
-    date_achievement_task DATE,
-    name_task CHAR(64),
-    file_task VARCHAR(512),
-    term_task INT,
-    FOREIGN KEY (id_project)
-        REFERENCES projects(id_projects),
-    FOREIGN KEY (id_users)
-        REFERENCES users(id_users)
-); 
-
-
-
 CREATE TABLE users (
     id_users INT AUTO_INCREMENT PRIMARY KEY,
     date_registration_users DATE,
@@ -35,3 +17,19 @@ CREATE TABLE users (
     password_users CHAR (255),
     contact_users CHAR(64)
 );
+
+CREATE TABLE task (
+    id_task INT AUTO_INCREMENT PRIMARY KEY,
+    id_users INT ,
+    id_projects INT,
+    date_create_task DATE,
+    date_achievement_task DATE,
+    name_task CHAR(64),
+    file_task VARCHAR(512),
+    term_task INT,
+    FOREIGN KEY (id_users)
+        REFERENCES users(id_users),
+    FOREIGN KEY (id_projects)
+        REFERENCES projects(id_projects)
+    
+); 
