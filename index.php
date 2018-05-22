3be18d8f65cfd1941a6954642d68c41bd60c2083
Ilya <mordar14@yadnex.ru>
2018-05-22 12:59:20 +0700

    Внёс правки по выборки из бд

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
 <?php
-require_once ('function.php');
-require_once ('mysql_helper.php');
 require_once ('init.php');
-require_once ('data.php');
-//Подлючаемся к бд
-$connect = mysqli_connect("localhost", "root","","business_is_all_right");
-if (!$connect) {
-    print("ОШибка подключения". mysqli_connect_error());
-}
-
 $user_id = 1;
 
 $categorys = catygorys_db($user_id,  $connect);
@@ -23,7 +14,7 @@ $filtered_tasks = [];
 $category_active = 0;
 if ($show_complete_tasks == 1){
     foreach ($tasks as  $task) {
-        if ($task['done'] == 'Нет') {
+        if ($task['done_task'] ==false ) {
             continue;
         }
         $filtered_tasks[] = $task;
--- init.php
+++ init.php
@@ -1,4 +1,11 @@
 <?php
 //Настройка проекта
+require_once ('function.php');
+require_once ('mysql_helper.php');
+require_once ('data.php');
+$connect = mysqli_connect("localhost", "root","","business_is_all_right");
+if (!$connect) {
+    print("ОШибка подключения". mysqli_connect_error());
+}
 date_default_timezone_set('Europe/Moscow');
 setlocale(LC_ALL, 'ru_RU');
