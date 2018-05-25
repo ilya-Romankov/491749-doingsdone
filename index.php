ae2395ad7414e7f9d5105d95265a61e116fdc7a9
Ilya <mordar14@yadnex.ru>
2018-05-26 02:44:54 +0700

    Активная категория

--- index.php
+++ index.php
@@ -4,21 +4,35 @@ $user_id = 1;
 $show_complete_tasks = rand(0, 1);
 $category_active = PROJECT_ALL;
 
-
+$categorys = catygorys_db($user_id, $connect); 
+$is_catygory_found = false;
 if (isset($_GET['id_projects'])) {
-    $category_active = $_GET['id_projects'];
-}
-else {
-    http_response_code(404); 
+    foreach ($categorys as $catygory) {
+        if ($_GET['id_projects'] == $catygory['id_projects']) {
+           $category_active = $_GET['id_projects'];
+           $is_catygory_found = true;
+           break;
+        }
+    }
+    if (!$is_catygory_found) { 
+        http_response_code(404);
+        echo output_page('template/404.php',[
+            'message' =>  'Данной категории не существует'
+        ]);
+        exit();
+    }
 }
 
-$categorys = catygorys_db($user_id, $connect);
 $tasks = task_db($user_id, $category_active, (bool)$show_complete_tasks, $connect);
 $count_task_by_user_id = get_count_tasks_by_user(1,$connect);
+
+$message = output_page($message, []);
 $index = output_page('template/index.php',[
     'show_complete_tasks' =>$show_complete_tasks ,
     'tasks' => $tasks
 ]);
+
+
 
 $layout = output_page('template/layout.php', [
         'name_page' =>  'Дела в порядке',
@@ -26,6 +40,6 @@ $layout = output_page('template/layout.php', [
         'category_active' => $category_active,
         'content' => $index,
         'tasks' => $tasks,
-        'count_task_by_user_id' => $count_task_by_user_id
+        'count_task_by_user_id' => $count_task_by_user_id 
 ]);
 print($layout);
